cae7143cc27d50e80b0a1a647d5a0247c7df2d1b
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-12 16:51:49 +1100

    added constraint of not able to click the seat if the seat is already selected and prevent the update process from happening

--- airplane.js
+++ airplane.js
@@ -2,16 +2,19 @@ const container = document.querySelector('.container')
 const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.querySelector('#count')
 const cost = document.querySelector('#cost')
-const price = document.querySelector('#airplane')
+const classSelected = document.querySelector('#airplane')
 
 
 seats.forEach((seat) => {
     seat.addEventListener('click', function () {
-        let countInt = parseInt(count.innerText)
-        let newCount = countInt + 1
-        count.innerText = newCount.toString()
-        seat.className = 'seat selected'
-        cost.innerText = (parseInt(price.value) * newCount).toString()
+        if (seat.className !== 'seat selected') {
+            let ticketPrice = +(classSelected.value)
+            let seatTotal = +count.innerText + 1
+            count.innerText = seatTotal.toString()
+            seat.className = 'seat selected'
+            cost.innerText = (ticketPrice * seatTotal).toString()
+        }
+
     })
 })
 
