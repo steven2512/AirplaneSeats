8ce8588f6aa591453a188912bd44803b74d8a0d8
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-12 17:37:21 +1100

    changed the prices of the seats once changed the airplane classes

--- airplane.js
+++ airplane.js
@@ -3,17 +3,19 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.querySelector('#count')
 const cost = document.querySelector('#cost')
 const classSelected = document.querySelector('#airplane')
-const ticketPrice = +(classSelected.value)
+
 
 function selectedUpdateCount() {
-
+    const ticketPrice = +(classSelected.value)
     let allSelectedSeats = document.querySelectorAll('.row .seat.selected')
     let totalSeats = allSelectedSeats.length
     count.innerText = totalSeats.toString()
     cost.innerText = (ticketPrice * totalSeats).toString()
 }
 
-
+classSelected.addEventListener('change', () => {
+    selectedUpdateCount()
+})
 
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat')) {
