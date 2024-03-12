0ac9136af848d0643575dd0406a5dc7038b5e625
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-12 16:42:13 +1100

    added the click event where clicking select the seat and update the total of seats and costs

--- airplane.js
+++ airplane.js
@@ -1,14 +1,17 @@
 const container = document.querySelector('.container')
-const seats = document.querySelectorAll('.seat')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.querySelector('#count')
 const cost = document.querySelector('#cost')
+const price = document.querySelector('#airplane')
 
 
 seats.forEach((seat) => {
     seat.addEventListener('click', function () {
         let countInt = parseInt(count.innerText)
-        let newValue = countInt + 1
-        count.innerText = newValue.toString()
-        debugger;
+        let newCount = countInt + 1
+        count.innerText = newCount.toString()
+        seat.className = 'seat selected'
+        cost.innerText = (parseInt(price.value) * newCount).toString()
     })
 })
+
